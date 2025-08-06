7e2422ba615f270cdc93587249ee357cc487cdef
anna <annavidaltur@gmail.com>
2025-08-06 20:43:34 +0200

    creuats list (wip)

--- server.js
+++ server.js
@@ -12,8 +12,33 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // endpoint que torna la llista de crosswords
 app.get("/api/crosswords", (req, res) => {
-  const files = fs.readdirSync(path.join(__dirname, "crosswords"));
-  res.json(files.map(f => f.replace(".json", "")));
+    const dir = path.join(__dirname, "crosswords");
+    try{
+        const dToday = new Date()
+        const strToday = dToday.toISOString().slice(0,10).replace(/-/g, "")
+
+        // filtrem .ipuz + els que siguen <= que el dia actual
+        const files = fs.readdirSync(dir)
+            .filter(f => f.endsWith(".ipuz") 
+                        && f.replace(".ipuz", "") <= strToday);
+
+        const puzzles = files.map(f => {
+            const fName = f.slice(0,f.length - 5) // llevem el .ipuz
+            const filePath = path.join(dir, f);
+            const content = JSON.parse(fs.readFileSync(filePath, "utf8"))
+
+            return {
+                id: fName,
+                date: `${fName.slice(0,4)}-${fName.slice(4,6)}-${fName.slice(6,8)}`, // 2025-08-10
+                title: content.title,
+                author: content.author
+            }
+        })
+        res.json(puzzles);
+    } catch (err) {
+        console.error("Error llegint els creuats: ", err)
+        res.status(500).json({error: "No s'han pogut carregar els creuats"});
+    }
 });
 
 // endpoint crossword concret
